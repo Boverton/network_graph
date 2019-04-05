2e5b07895c8e4c36f0fd87366aa5d6ddec22290b
brandonoverton <igrproductions@gmail.com>
2019-04-05 10:43:27 -0500

    Moving GraphContainer over to class Component instead of functional   need access to lifeCycles and only update when new props are provided.
      Also includes
        implementation of overlay on the graph before nodes added
        UI changes to graph config
           height of graph
           width of graph
           size of node
           fontSize of label

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Graph} from "react-d3-graph";
 import PropTypes from 'prop-types';
 
+export default class GraphContainer extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return JSON.stringify(this.props) !== JSON.stringify(nextProps);
+    }
 
-const GraphContainer = (props) => {
+    render() {
+        // the graph configuration, you only need to pass down properties
+        // that you want to override, otherwise default ones will be used
+        const myConfig = {
+            nodeHighlightBehavior: false,
+            height: 550,
+            width: 600,
+            node: {
+                color: 'lightgreen',
+                size: 400,
+                fontSize: 15,
+            },
+        };
 
-    // the graph configuration, you only need to pass down properties
-    // that you want to override, otherwise default ones will be used
-    const myConfig = {
-        nodeHighlightBehavior: false,
-        node: {
-            color: 'lightgreen',
-            size: 120,
-        },
-    };
-
-    return (
-        <Graph
-            id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-            data={props.data}
-            config={myConfig}
-            onClickNode={props.onClickNode}
-        />
-    )
+        return (
+            <div id="graph-container">
+                <div
+                    id="overlay"
+                    className={this.props.data.nodes.length === 1 ? "show" : ""}
+                >
+                    <div id="instructions">
+                        <span className="title">
+                            Add a node above to start your graph
+                        </span>
+                        <span>
+                            You can do the following:
+                        </span>
+                        <ul>
+                            <li>
+                                Drag and drop nodes
+                            </li>
+                            <li>
+                                Zoom in and out
+                            </li>
+                            <li>
+                                Create edges between 2 nodes
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <Graph
+                    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+                    data={this.props.data}
+                    config={myConfig}
+                    onClickNode={this.props.onClickNode}
+                />
+            </div>
+        )
+    }
 };
-
-export default GraphContainer;
 
 GraphContainer.propTypes = {
   data: PropTypes.object.isRequired,
